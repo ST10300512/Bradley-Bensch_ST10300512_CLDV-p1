5a1f109a700dc4c4804a23946eb48d261121f2d0
Bradley Bensch <ST10300512@vcconnect.edu.za>
2024-08-10 13:37:07 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10300512_CLDVPOE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
