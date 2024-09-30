51cb87605b52d83f6a3d6d757368b993807fba93
Bradley Bensch <ST10300512@vcconnect.edu.za>
2024-09-30 13:14:07 +0200

    Added Function Assets to my project

---
+++ ProcessQueueMessage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace ST10300512_CLDVPOE
+{
+    public class ProcessQueueMessage
+    {
+        private readonly ILogger<ProcessQueueMessage> _logger;
+
+        public ProcessQueueMessage(ILogger<ProcessQueueMessage> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("ProcessQueueMessage")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
---
+++ UploadBlob.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+
+namespace ST10300512_CLDVPOE
+{
+    public class UploadBlob
+    {
+        private readonly ILogger<UploadBlob> _logger;
+
+        public UploadBlob(ILogger<UploadBlob> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("UploadBlob")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
