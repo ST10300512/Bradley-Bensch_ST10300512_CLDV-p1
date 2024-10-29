d7f69356e104d86cad7d6f5ab3dd38937bd4bdb8
Bradley Bensch <ST10300512@vcconnect.edu.za>
2024-10-29 16:40:12 +0200

    Created new classes for the connection string for the database

---
+++ Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ST10300512_CLDVPOE.Models
+{
+    public class Image : Controller
+    {
+        public static string con_string = "Server=tcp:st10300512-poecldv.database.windows.net,1433;Initial Catalog=st10300512server-poecldv;Persist Security Info=False;User ID=bradleybenschst10300512D;Password=Rocky@4ngle;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public static SqlConnection con = new SqlConnection(con_string);
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
